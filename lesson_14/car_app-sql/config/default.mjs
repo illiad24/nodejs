30242b80a44eb1f5ef9af07a5b71858135ecb790
web developer <119691181+illiad24@users.noreply.github.com>
2024-12-27 18:34:41 +0200

    Delete lesson_14/car_app-sql directory

--- lesson_14/car_app-sql/config/default.mjs
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-export default Object.freeze({
-    host: process.env.SQL_HOST,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DATABASE,
-})
--- lesson_14/car_app-sql/routes/index.mjs
+++
@@ -1,8 +0,0 @@
-import MainController from '../controllers/mainController.mjs'
-import { Router } from 'express'
-const router = Router()
-
-//---- обробка шляху '/'
-router.get('/', MainController.info)
-
-export default router
